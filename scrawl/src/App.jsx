e2d7d088a84ae185314ab5a79a96e94e7a6c6bc6
Faith <felominofaith@gmail.com>
2026-03-18 14:52:26 +0800

    added uselayout

--- scrawl/src/App.jsx
+++ scrawl/src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import { useLayoutEffect, useState } from 'react'
 import './App.css'
 
+
+
+
 function App() {
+
+  useLayoutEffect(() => {
+    const canvas = document.getElementById('canvas')
+    const ctx = canvas.getContext('2d')
+
+    ctx.fillStyle = 'red'
+    ctx.fillRect(10, 10, 100, 100)
+
+  });
   
   return (
     <canvas id="canvas" width={window.innerWidth} height={window.innerHeight} >Canvas</canvas>
-    
   );
 }
 
