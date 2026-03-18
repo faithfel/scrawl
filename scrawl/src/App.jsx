2ca37e7170f4fe4a884582010a92a1075d3be97e
Faith <felominofaith@gmail.com>
2026-03-18 15:25:25 +0800

    mouse

--- scrawl/src/App.jsx
+++ scrawl/src/App.jsx
@@ -5,6 +5,9 @@ import rough from 'roughjs/bundled/rough.esm.js';
 const generator = rough.generator();
 
 function App() {
+  const [elements, setElements] = useState([]);
+  const[drawing, setDrawing] = useState(false);
+
 
   useLayoutEffect(() => {
     const canvas = document.getElementById('canvas')
@@ -12,12 +15,40 @@ function App() {
 
     const roughCanvas = rough.canvas(canvas);
     const rect = generator.rectangle(10, 10, 100, 100,);
+    const line = generator.line(10, 10, 110, 110,);
     roughCanvas.draw(rect);
- 
+    roughCanvas.draw(line);
+
   });
 
+  const handleMouseDown = (event) => {
+    setDrawing(true);
+  }
+  
+  const handleMouseMove = (event) => {
+    if (!drawing) return;
+
+    const {clientX, clientY} = event;
+    console.log(clientX, clientY);
+  }
+
+  const handleMouseUp = (event) => {
+    setDrawing(false);
+  }
+
+
   return (
-    <canvas id="canvas" width={window.innerWidth} height={window.innerHeight} >Canvas</canvas>
+    <canvas 
+    
+    id="canvas" 
+    width={window.innerWidth} 
+    height={window.innerHeight} 
+    onMouseDown={handleMouseDown}
+    onMouseMove={handleMouseMove}
+    onMouseUp={handleMouseUp}
+    >
+    Canvas
+    </canvas>
   );
 }
 
